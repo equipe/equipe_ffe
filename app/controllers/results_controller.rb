cd4763ba7e162437e80b859381892a7f99fc4a8e
Jon Stenqvist <jon.stenqvist@equipe.com>
2016-09-04 14:56:29 +0200

    Return accepted

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -3,7 +3,7 @@ class ResultsController < ApplicationController
   def create
     results = JSON.parse(request.body)
     Rails.logger.info "GOT #{results.inspect}"
-    head :created
+    head :accepted
   end
 
 end
