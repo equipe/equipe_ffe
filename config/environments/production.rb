50eb6bf7acd8525976bb8c52c500360176ae5535
Jon Stenqvist <jon.stenqvist@equipe.com>
2016-11-28 03:49:54 +0100

    Don't tag logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,10 +58,10 @@ Rails.application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
+  config.active_support.deprecation = :log
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  # config.log_formatter = ::Logger::Formatter.new
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
