1b8553749dad00fbba8a6748f02148b7d94f5953
Jon Stenqvist <jon.stenqvist@equipe.com>
2018-03-13 13:26:59 +0100

    updated foreign id's to string

--- app/models/equipe/entries.rb
+++ app/models/equipe/entries.rb
@@ -7,7 +7,7 @@ class Equipe::Entries
   def as_json(*)
     {
       show: {
-        id: show.id,
+        id: show.id.to_s,
         name: show.name,
         starts_on: show.starts_on,
         ends_on: show.ends_on,
@@ -27,23 +27,23 @@ class Equipe::Entries
   attr_reader :show
 
   def competitions
-    show.competitions.select(:id, :competition_no, :name, :starts_on, :discipline, :horse_pony, :judgement_id, :late_entry_fee, 'true AS randomized', "'N'::text AS status", "'all_categories_together'::text AS category_merge")
+    show.competitions.select('id::text AS id', :competition_no, :name, :starts_on, :discipline, :horse_pony, :judgement_id, :late_entry_fee, 'true AS randomized', "'N'::text AS status", "'all_categories_together'::text AS category_merge")
   end
 
   def people
-    show.people.select(:id, :first_name, :last_name, :licence, 'birthday AS person_no', "'FRA'::text AS country", :official, :club_id)
+    show.people.select('id::text AS id', :first_name, :last_name, :licence, 'birthday AS person_no', "'FRA'::text AS country", :official, 'club_id::text AS club_id')
   end
 
   def clubs
-    show.clubs.select(:id, :name, 'clubs.ffe_id AS logo_id', "'ffe'::text AS logo_group")
+    show.clubs.select('id::text AS id', :name, 'clubs.ffe_id AS logo_id', "'ffe'::text AS logo_group")
   end
 
   def horses
-    show.horses.select(:id, :licence, :name, :sire, :dam, :dam_sire, :born_year, :color, :breed, :race, :height, :sex, :category)
+    show.horses.select('id::text AS id', :licence, :name, :sire, :dam, :dam_sire, :born_year, :color, :breed, :race, :height, :sex, :category)
   end
 
   def entries
     # Start no and position should only be given when the start list is already randomized
-    show.entries.joins(:competition).select(:id, 'competitions.competition_no AS competition_no', :start_no, 'start_no AS position', :rider_id, :horse_id, 'rider_id AS payer_id')
+    show.entries.joins(:competition).select('id::text AS id', 'competitions.competition_no AS competition_no', :start_no, 'start_no AS position', 'rider_id::text AS rider_id', 'horse_id::text AS horse_id', 'rider_id::text AS payer_id')
   end
 end
